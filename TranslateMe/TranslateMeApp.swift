e6d6e514106d9a7906088112969692ad1b7f9458
benuhdrill <bgmach2023@fau.edu>
2024-11-11 11:58:36 -0500

    Initial Commit

---
+++ TranslateMe/TranslateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeApp.swift
+//  TranslateMe
+//
+//  Created by Ben Gmach on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
