66f560ab4563a874fed06eae803cc76d574b2e1a
benuhdrill <bgmach2023@fau.edu>
2024-11-11 16:02:17 -0500

    First commit

---
+++ TranslateMe/Translation.swift
@@ -0,0 +1,18 @@
+//
+//  Translation.swift
+//  TranslateMe
+//
+//  Created by Ben Gmach on 11/11/24.
+//
+
+import SwiftUI
+
+struct Translation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Translation()
+}
---
+++ TranslateMe/tempCodeRunnerFile.swift
@@ -0,0 +1,18 @@
+//
+//  TempCodeRunnerFile.swift
+//  TranslateMe
+//
+//  Created by Ben Gmach on 11/11/24.
+//
+
+import SwiftUI
+
+struct TempCodeRunnerFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TempCodeRunnerFile()
+}
